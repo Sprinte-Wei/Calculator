92651cea6ddbf5fd69a2233b3a31e282919af898
iss2016302580238 <1320018234@qq.com>
2018-09-26 09:52:46 +0800

    modify UI

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import sample.lexer.Lexer;
+import sample.lexer.LexicalException;
 import sample.lexer.Token;
 import sample.parser.Parser;
 
@@ -17,28 +18,34 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 360, 590));
+        primaryStage.setScene(new Scene(root, 360, 680));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-        //launch(args);
+        launch(args);
 
         //Added by 魏春光
         Scanner sc=new Scanner(System.in);
+
         while (true){
             //词法分析
-            Lexer l = new Lexer(sc.next());
-            for(Token t : l.getTokens()){
-                System.out.println(t);
-            }
+            try{
+                Lexer l = new Lexer(sc.next());
+                for(Token t : l.getTokens()){
+                    System.out.println(t);
+                }
 
-            //语法分析
-            Parser p = new Parser();
-            if(p.readString(l.getTokens())) System.out.println("输入句子形式正确");
-            else System.out.println("输入句子形式错误");
+                //语法分析
+                Parser p = new Parser();
+                if(p.readString(l.getTokens())) System.out.println("输入句子形式正确");
+                else System.out.println("输入句子形式错误");
+            }
+            catch (LexicalException e){
+                e.printStackTrace();
+            }
         }
 
     }
